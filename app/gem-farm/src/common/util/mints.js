92ee7ddbaa8e7cc10386c7ff3f22151a2cb7215e
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-05-09 22:03:57 -0400

    updated hash list

--- app/gem-farm/src/common/util/mints.js
+++ app/gem-farm/src/common/util/mints.js
@@ -255,5 +255,6 @@ export const mints = [
   "8fFyWFZzDCuq7i4FggDFEbhKkk1WcsqfwDoW7jxZrexV",
   "3Mjjd3rkjNbkuAXUhTiJqMy9KRHpFPZUuz5H4P3FHg1a",
   "39ZkFfbEiszTkpVpZJhyRKc1bGw1G39zhAMQqSk3sLBD",
-  "EMTshsoeDHMFzvgNz3kCu9q4VpxgiTb8fWSDGcNuwBgF"
+  "EMTshsoeDHMFzvgNz3kCu9q4VpxgiTb8fWSDGcNuwBgF",
+  "Ai98AZCLK8m9uMTJE8p4buVYnf6twmnfqGW9FBtycC2Q"
 ]
